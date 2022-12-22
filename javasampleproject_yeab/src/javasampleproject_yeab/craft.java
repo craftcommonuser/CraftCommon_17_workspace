b9c480bae84b6a0d1e524e70bf0574e0e88d18d0
yeabm <120437783+yeabm@users.noreply.github.com>
2022-12-22 01:07:17 -0500

    trying eclipse 3

--- javasampleproject_yeab/src/javasampleproject_yeab/craft.java
+++ javasampleproject_yeab/src/javasampleproject_yeab/craft.java
@@ -5,8 +5,6 @@ public class craft {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		
-	}
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 }ss
