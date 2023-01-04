9b6d1b339a26675ac3ad858080fff6b03d6cc25a
Yibeltaluser <120437921+Yibeltaluser@users.noreply.github.com>
2023-01-03 23:31:14 -0500

    I create mydb

---
+++ mydb_yibeltal.sql
@@ -0,0 +1,13 @@
+select product_)id, name Unit_price
+select max (quantity_ in_stok = (select min )
+from products
+
+select max (quantity_inin_stack)  as maxquantity
+from produxts
+where unit_price >2
+
+select count (productid) as numberofproduct
+from products
+
+select avg(unitprice) as unutprice
+from product
