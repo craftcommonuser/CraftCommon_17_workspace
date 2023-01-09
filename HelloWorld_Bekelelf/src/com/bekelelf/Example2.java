139b541c3d67c71daf0181ca6a40c25b9ec86d24
Tsion2001 <120601191+Tsion2001@users.noreply.github.com>
2023-01-08 20:32:09 -0800

    bata my db

---
+++ HelloWorld_Bekelelf/src/com/bekelelf/Example2.java
@@ -0,0 +1,14 @@
+package com.bekelelf;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+	
+	
+}
---
+++ Helloworld/src/com/icraft/example2.java
@@ -0,0 +1,13 @@
+package com.icraft;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("print something");
+	       System.out.println("print something");
+	       System.out.println("print something");
+	       System.out.println("print something");
+	}
+
+}
