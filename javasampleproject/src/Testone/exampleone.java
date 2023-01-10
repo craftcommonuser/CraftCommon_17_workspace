513ebd3af7b7fb9e67d124529d5b4f665238e739
Woinshet-w <woinib16@gmail.com>
2023-01-09 21:12:09 -0800

    commit from eclipse

---
+++ javasampleproject/src/Testone/exampleone.java
@@ -0,0 +1,12 @@
+package Testone;
+
+public class exampleone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("print something");
+
+	}
+
+}
