b0ba60719455c097d55245221e401d586298c9e2
Amanag12 <120347495+Amanag12@users.noreply.github.com>
2023-01-12 22:47:14 -0500

    Java push

---
+++ Craft_Common_1/src/Com/Day2/AmanAg.java
@@ -0,0 +1,28 @@
+package Com.Day2;
+
+public class AmanAg {
+	static int AA= 23; 
+	system.out.println();
+	
+	public void methodone(){
+		
+		int age = 25;
+		System.out.println("Age of M1: " + age);
+	}
+	
+    public void methodtwo(){
+		
+	 String name = "Aman";
+		System.out.println("Age of M2: " + name);
+		
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AmanAg Aa = new AmanAg();
+		Aa.methodone();
+		Aa.methodtwo();
+
+	}
+
+}
