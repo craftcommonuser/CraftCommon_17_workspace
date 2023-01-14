99c64a5d985b08d776e1cdd71312a03b24e6263b
bereketelias <120437933+bereketelias@users.noreply.github.com>
2023-01-14 13:20:52 -0500

    Java Sample

---
+++ project1/src/project11/Example1.java
@@ -0,0 +1,12 @@
+package project11;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is First Java Exercise");
+		System.out.println("This is First Java Exercise1");
+
+	}
+
+}
