fe9b6155d6f5fabbfb8fcfc572cd2c58428999bc
bereketelias <120437933+bereketelias@users.noreply.github.com>
2023-01-15 22:20:58 -0500

    update

--- project1/src/project11/Example1.java
+++ project1/src/project11/Example1.java
@@ -5,6 +5,8 @@ public class Example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is First Java Exercise");
+		System.out.println("This is First Java Exercise");
+		System.out.println("This is First Java Exercise");
 		
 
 	}
