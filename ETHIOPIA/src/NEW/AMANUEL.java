81cd3ebf4fc61addc03a23300e6c676700539551
AMANUELALELIGN <120440187+AMANUELALELIGN@users.noreply.github.com>
2023-01-17 13:54:40 -0500

    HELLO

---
+++ ETHIOPIA/src/NEW/AMANUEL.java
@@ -0,0 +1,14 @@
+package NEW;
+
+public class AMANUEL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = ("Aamanuel");
+		String lastName =("Asmare");
+		System.out.println("firstName"+firstName);
+		System.out.println("lastName:"+lastName);
+
+	}
+
+}
--- ETHIOPIA/src/module-info.java
+++ ETHIOPIA/src/module-info.java
@@ -5,5 +5,5 @@
  * @author amanu
  *
  */
-module TestOne {
+module ETHIOPIA {
 }
