49c9cd4501907aeeb5bf8fa4cfecb8a11ed7d7b1
AMANUELALELIGN <120440187+AMANUELALELIGN@users.noreply.github.com>
2023-01-17 01:11:24 -0500

    NEW COMMIT

---
+++ projectone/src/projectone/another.java
@@ -0,0 +1,14 @@
+package projectone;
+
+public class another {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Amanuel";
+		String lastName = "Asmare";
+		System.out.println("my FiristName "+ firstName);
+		System.out.println("my LastName" + lastName);
+
+	}
+
+}
