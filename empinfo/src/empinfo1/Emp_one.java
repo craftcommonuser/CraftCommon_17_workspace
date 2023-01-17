1af326f7c6e54845fd6cda050669585f831245bd
kasey644githubuser <yabyouk@gmail.com>
2023-01-17 18:01:51 -0500

    test sample.

---
+++ empinfo/src/empinfo1/Emp_one.java
@@ -0,0 +1,21 @@
+package empinfo1;
+
+public class Emp_one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+    String name = "John smith";
+    int salary = 3000;
+    double bonus = 500;
+    double total_salary = salary + bonus;
+
+    System.out.println("name of the employee  : " + name);
+    System.out.println("salary of the employee  : " + salary);
+    System.out.println("bonus of the employee : " + bonus);
+    System.out.println("total salary of the employee: " + total_salary);
+
+  }
+
+}
+
