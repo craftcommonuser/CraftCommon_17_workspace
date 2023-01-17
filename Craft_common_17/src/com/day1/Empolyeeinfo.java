9c8f5325f787286ee1d5497adfa7dfea4bf19fda
ttefferra <ttefferra@gmail.com>
2023-01-16 21:06:54 -0500

    initial commit

---
+++ Craft_common_17/src/com/day1/Empolyeeinfo.java
@@ -0,0 +1,22 @@
+package com.day1;
+
+import java.util.Scanner; 
+
+public class Empolyeeinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("pls enter your name:");
+        String empName = in.next();
+        
+        System.out.println("pls enter your last name");
+        String empLastName = in .next() ;
+        
+        
+        		
+	}
+
+}
