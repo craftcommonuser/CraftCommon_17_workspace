9bbaba62635b7fe6fab3fcf71feeedb9a7fcc124
AMANUELALELIGN <120440187+AMANUELALELIGN@users.noreply.github.com>
2023-01-17 01:42:03 -0500

    new

--- projectone/src/projectone/another.java
+++ projectone/src/projectone/another.java
@@ -5,7 +5,7 @@ public class another {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String firstName = "Amanuel";
-		String lastName = "Asmare";
+		String lastName = "Asmaree";
 		System.out.println("my FiristName "+ firstName);
 		System.out.println("my LastName" + lastName);
 
