465083a0832470e17c31ea6598533bb3e98938be
Asmare1219 <120425854+Asmare1219@users.noreply.github.com>
2023-01-17 12:06:36 -0800

    hellogit

---
+++ washington2023/src/washington2023/Asmare.java
@@ -0,0 +1,25 @@
+package washington2023;
+
+public class Asmare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String firstName ="Asmare";
+		String lastName ="Hunegnaw";
+		int age = 36;
+		double salary = 2000.80;
+		
+		System.out.println("my firstName:" + firstName);
+		System.out.println("my lastName:" +lastName );
+		System.out.println("my age:" + age);
+		System.out.println("my salary"+ salary);
+		
+		
+		
+		
+		
+		
+	}
+
+}
