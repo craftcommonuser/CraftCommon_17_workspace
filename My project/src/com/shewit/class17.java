2e577fdc6d252f1fc80a88724660a9ccf1833eb3
mdidana <120535365+mdidana@users.noreply.github.com>
2023-01-16 23:49:05 -0600

    Revert "Revert "Updated""
    This reverts commit acac9064f62ffb84621842fbc2787585b52da4d1.

--- My project/src/com/shewit/class17.java
+++ My project/src/com/shewit/class17.java
@@ -14,6 +14,15 @@ public class class17 {
       System.out.println("My LastName");
       System.out.println("write New LastName");
       System.out.println("write My New LastName");
+      System.out.println("My Name");
+      System.out.println("write Myname");
+      System.out.println("Print My Name");
+      System.out.println("Print My LastName");
+      System.out.println("new My LastName");
+      System.out.println("Print My Name");
+      System.out.println("My LastName");
+      System.out.println("write New LastName");
+      System.out.println("write My New LastName");
      
 	}
 
