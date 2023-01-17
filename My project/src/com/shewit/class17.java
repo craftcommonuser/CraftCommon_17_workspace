88914b4ef34d3725f1c99dba1bd7fb1972c639a5
shewit1G <snha309@gmail.com>
2023-01-16 22:17:51 -0500

    Merge branch 'main' of https://github.com/craftcommonuser/CraftCommon_17_workspace.git into main

--- My project/src/com/shewit/class17.java
+++ My project/src/com/shewit/class17.java
@@ -11,7 +11,7 @@ public class class17 {
       System.out.println("Print My LastName");
       System.out.println("Print My LastName");
       System.out.println("Print My Name");
-      System.out.println("Print My LastName");
+      System.out.println("My LastName");
       System.out.println("write New LastName");
       System.out.println("write My New LastName");
      
