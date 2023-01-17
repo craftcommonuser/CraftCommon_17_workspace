40ee950aecdb2f1d33a265ab15bc7bbff0168e29
AMANUELALELIGN <120440187+AMANUELALELIGN@users.noreply.github.com>
2023-01-17 13:55:54 -0500

    new try

--- ETHIOPIA/src/NEW/AMANUEL.java
+++ ETHIOPIA/src/NEW/AMANUEL.java
@@ -5,7 +5,7 @@ public class AMANUEL {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String firstName = ("Aamanuel");
-		String lastName =("Asmare");
+		String lastName =("Asmaree");
 		System.out.println("firstName"+firstName);
 		System.out.println("lastName:"+lastName);
 
