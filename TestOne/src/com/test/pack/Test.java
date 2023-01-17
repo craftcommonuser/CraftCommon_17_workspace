ab93ee8979aa7e2b92e0798f16f2064cf348438a
AMANUELALELIGN <120440187+AMANUELALELIGN@users.noreply.github.com>
2023-01-17 13:05:34 -0500

    my initial commit from eclipse

---
+++ TestOne/src/com/test/pack/Test.java
@@ -0,0 +1,12 @@
+package com.test.pack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Print Hello");
+
+	}
+
+}
