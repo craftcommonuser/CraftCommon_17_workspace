d552af2dd6da50ff848e1f9ba2f20053807e29f4
Yibeltaluser <120437921+Yibeltaluser@users.noreply.github.com>
2023-01-17 00:27:38 -0500

    data type commit

---
+++ employeeData/src/craft/employee/Data/dataType.java
@@ -0,0 +1,19 @@
+package craft.employee.Data;
+
+import java.util.Scanner;
+
+public class dataType {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println ("enter name");
+		String name = in.next();
+		System.out.println ("enter age");
+		int age = in.nextInt();
+		
+		System.out.println ("enter salary");
+		double salary = in.nextDouble();
+		
+	}
+
+}
