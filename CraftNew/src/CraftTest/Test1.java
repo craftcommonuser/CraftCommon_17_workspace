e4d1337d005c160321c0e3d6d33e23c5341576f1
Dawit <117766446+Dave1501@users.noreply.github.com>
2023-01-17 10:06:04 -0500

    Initial commit2

---
+++ CraftNew/src/CraftTest/Test1.java
@@ -0,0 +1,24 @@
+package CraftTest;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "jonny";
+		String lastName = "James";
+		int age = 34;
+		double salary =   2000.45;
+		double bonus = 100;
+		
+		double totalSalary = salary + bonus;
+		
+		System.out.println("Student FirstName: "+ firstName);
+		System.out.println("Student LastName: " +lastName);
+		System.out.println("Student Age: " + age);
+		System.out.println("Student Salary: " + salary);
+		System.out.println("Student Bonus: " + bonus);
+		System.out.println("Student Total salary: " + totalSalary);
+	
+	}
+
+}
