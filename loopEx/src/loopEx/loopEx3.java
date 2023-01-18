84e5b4847d2d09e69c41e27a58107ff98ae4e120
Tsega-Teka <tsegaassefa64@gmail.com>
2023-01-17 23:37:23 -0600

    More loop ex for TsegaTeka

---
+++ loopEx/src/loopEx/loopEx3.java
@@ -0,0 +1,27 @@
+package loopEx;
+
+public class loopEx3 {
+
+	public static void main(String[] args) {
+		
+
+		for(int k=1; k<=5; k++) {
+		
+		for(int i =1; i<=5; i++) {
+		System.out.print(i +" ");
+		
+	}
+		System.out.println();
+		}
+		
+		System.out.println("-------------------------------");
+	
+		
+		for(int x= 1;x<=5; x++){
+		
+		System.out.println(x + " " + x +" " + x + " " + x + " "+x);
+			
+		}
+	
+ }
+}
