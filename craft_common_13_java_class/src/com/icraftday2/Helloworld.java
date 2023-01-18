d0750b1199e0373a755dce314657d5a49245b754
aster161 <120440076+aster161@users.noreply.github.com>
2023-01-18 00:51:13 -0500

    hello

---
+++ craft_common_13_java_class/src/com/icraftday2/Helloworld.java
@@ -0,0 +1,29 @@
+package com.icraftday2;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          String firstName="Adam";
+          String lastName ="James";
+          int age =34;
+          double salary=200000.45;
+          double bonus=100;
+          double totalsalary = salary+bonus;
+          
+          
+          System.out.println(firstName);
+          System.out.println(lastName);
+          System.out.println(age);
+          System.out.println(salary);
+          System.out.println(bonus);
+          
+          System.out.println("student FirstName: " + firstName);
+          System.out.println("student LasttName: " + lastName);
+          System.out.println("student Age: " + age);
+          System.out.println("student Salary: " + salary);
+          System.out.println("student Bonus: " + bonus);
+          System.out.println("student Total salary: " + totalsalary);
+	}
+
+}
