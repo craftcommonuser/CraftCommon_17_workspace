abf850137278bdb68abbb859e08ab694240ff193
chalkulih <chalkulihwubetuc@gmail.com>
2023-01-19 11:53:04 -0600

    commit from chalkulih

---
+++ HelloWorld_Chalkulih/src/com/group_d/helloJavaProgram.java
@@ -0,0 +1,55 @@
+package com.group_d;
+
+public class helloJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("Welcome to World of Java");
+	
+int age;
+age = 25;
+	
+System.out.println(age);
+
+char gender;
+gender = 'M';
+System.out.println(gender);
+
+
+// byte
+
+byte x = 123;
+System.out.println(gender);
+
+//short
+short y = 7645;
+
+System.out.println(y);
+
+// Int
+
+
+// long 
+ long L = 12345678; 
+ System.out.println(L);
+	
+	// double 
+ double D = 45678.0;
+ 
+ System.out.println(D);
+ 
+ 
+ 
+ // float
+ float f= 45678.567f;
+ 
+ System.out.println(f);
+ 
+ // boolean
+ boolean m = true; 
+ 
+ System.out.println(m);
+	}
+
+}
