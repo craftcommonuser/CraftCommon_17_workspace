2b9d7084a0113d9b800369428cae4e29c87321f9
Tamerat <real.wise2022@gmail.com>
2023-01-20 01:31:45 -0500

    Tamerat's Day8 Commit

---
+++ Icraft_Java_Project/src/whileLoop/ForLoop.java
@@ -0,0 +1,12 @@
+package whileLoop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <=11; i++) {
+			System.out.print("The value of i is:" + i);
+			System.out.println("\n");
+		}
+	}
+}
