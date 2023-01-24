0501ad8e2e8d94edd99fc39fd4b1c97db83a3332
Yibeltaluser <120437921+Yibeltaluser@users.noreply.github.com>
2023-01-23 23:50:15 -0500

    First commit

---
+++ FirstPractice/src/craft/java/GrupdJava.java
@@ -0,0 +1,17 @@
+package craft.java;
+
+public class GrupdJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Jeo";
+		String lastName = "Yoha";
+		int age = 30;
+		
+		System.out.println("First Name" + firstName);
+		System.out.println("Last Name" + lastName);
+		System.out.println("Age" + age);
+
+	}
+
+}
