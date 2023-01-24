4c646569bfd14f5dec9b09e7612e7d2fd45f684e
elshadayeeshete <110870938+elshadayeeshete@users.noreply.github.com>
2023-01-23 22:55:14 -0800

    Encapsulation

---
+++ Encapsulation/src/EncapsulationDemo1/getterStudent.java
@@ -0,0 +1,24 @@
+package EncapsulationDemo1;
+
+public class getterStudent {
+	// all variables must be private
+	private String firstName = "Jamie";
+	private String lastName = "Foxx";
+	private int age = 45;
+	private String Collage = "Natural Science";
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getCollage() {
+		return Collage;
+	}
+	
+	
+}
