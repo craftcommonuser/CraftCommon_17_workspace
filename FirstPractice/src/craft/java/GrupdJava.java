f5d0c9af79446d071b9f4850c63dbd92b6b31d1b
Yibeltaluser <120437921+Yibeltaluser@users.noreply.github.com>
2023-01-23 23:57:24 -0500

    Merge branch 'main' of https://github.com/craftcommonuser/CraftCommon_17_workspace.git into main

--- FirstPractice/src/craft/java/GrupdJava.java
+++ FirstPractice/src/craft/java/GrupdJava.java
@@ -4,9 +4,10 @@ public class GrupdJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String firstName = "Jeo";
+		String firstName = "Jeo";// first name
 		String lastName = "Yoha";
 		int age = 30;
+		
 		
 		System.out.println("First Name" + firstName);
 		System.out.println("Last Name" + lastName);
