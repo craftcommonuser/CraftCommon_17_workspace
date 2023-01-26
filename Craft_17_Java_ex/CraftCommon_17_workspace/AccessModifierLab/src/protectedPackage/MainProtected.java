6efdad70e253bd696f3b2760641cee4b19c3e93d
elshadayeeshete <110870938+elshadayeeshete@users.noreply.github.com>
2023-01-25 21:34:52 -0800

    commit

---
+++ Craft_17_Java_ex/CraftCommon_17_workspace/AccessModifierLab/src/protectedPackage/MainProtected.java
@@ -0,0 +1,15 @@
+package protectedPackage;
+
+public class MainProtected extends protectedDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		protectedDemo m3 = new protectedDemo();
+		m3.msg();   //classname. methodname 
+		
+	
+
+	}
+
+}
