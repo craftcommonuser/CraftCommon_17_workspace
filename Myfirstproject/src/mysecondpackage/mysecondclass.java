ae9746f5d71b0502425fe2a0758b7e55acff0305
Meronchibsa <120437892+Meronchibsa@users.noreply.github.com>
2023-01-30 15:34:06 -0800

    2ndcommit

---
+++ Myfirstproject/src/mysecondpackage/mysecondclass.java
@@ -0,0 +1,22 @@
+package mysecondpackage;
+
+public class mysecondclass {
+
+	public void varone() {
+	int i = 25;
+	System.out.println("Value of i:" + i);
+	}
+	
+	public void vartwo() {
+		int j = 45;
+		System.out.println("Value of j:" + j);
+		}
+
+	public static void main(String[] args) {
+		
+mysecondclass demo = new mysecondclass();
+demo.varone();
+demo.vartwo();
+
+}
+}
